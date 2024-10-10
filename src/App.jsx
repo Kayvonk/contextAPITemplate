c4759bb155fab2e42da157be720d6feebfd3a69c
Kayvon Kazemini <kayvonk@gmail.com>
2024-10-09 22:54:17 -0600

    added div in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { useContext, useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Counter from './components/Counter'
-import { GlobalData } from './Contexts/GlobalContext'
+import { useContext, useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import { GlobalData } from "./Contexts/GlobalContext";
 
 function App() {
+  const { user } = useContext(GlobalData);
 
-const { user } = useContext(GlobalData)
-
-
-useEffect(() => {
-console.log("user:", user);
-}, [user])
+  useEffect(() => {
+    console.log("user:", user);
+  }, [user]);
 
   return (
     <>
       <Counter></Counter>
-      {user?.name}
+      <div>{user?.name}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
